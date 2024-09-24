ec3dc17e8ffb7c8b8840cd55d2b8254c2d59fc29
unknown <jarhodee@gmail.com>
2024-09-24 12:08:43 +0100

    project completed

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-console.log(randomNumber1);
+var randomNumber2 = Math.floor(Math.random () * 6) + 1;
+
+var randomDiceImage1 = "./images/" + "dice" + randomNumber1 + ".png";
+var randomDiceImage2 = "./images/" + "dice" + randomNumber2 + ".png";
+
+document.getElementById("dice-left").setAttribute("src", randomDiceImage1);
+document.getElementById("dice-right").setAttribute("src", randomDiceImage2);
+
+var resultText;
+if (randomDiceImage1 > randomDiceImage2) {
+    resultText = "🚩Player 1 Wins";
+} else if (randomDiceImage1 < randomDiceImage2) {
+    resultText = "Player 2 Wins🚩";
+} else {
+    resultText = "It's a Draw!"; 
+}
+
+document.getElementById("result").textContent = resultText;
+
+document.getElementById("refresh-btn").addEventListener("click", function() {
+    location.reload()
+});
